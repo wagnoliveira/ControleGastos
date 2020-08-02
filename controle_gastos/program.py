a27f2760e471527d30863497f9a1c0f910294093
Wagner <wagnoliveira.txt@gmail.com>
2020-08-01 22:09:49 -0300

    Versão 0
    Tentei criar HTML, fracassei.
    Tentei fazer o script, fracassei.
    Abri um arquivo em python pra testar a lógica e deu certo.

---
+++ controle_gastos/program.py
@@ -0,0 +1,5 @@
+renda = float(input('Insira sua renda mensal: R$ '))
+
+print(f'Sua renda é de {renda:.2f}.')
+print(f'RENDA DE AGORA {renda * 0.70:.2f}')
+print(f'RENDA PARA INVESTIMENTO {renda * 0.30:.2f}')
---
+++ controle_gastos/script.js
@@ -0,0 +1,13 @@
+var rend = document.getElementById('rendimentos')
+    var tot = Number(rend)
+    var agora = tot * 0.70
+    var invest = tot * 0.70
+
+function calcular() {
+    item.creatitem('option')
+    
+    
+
+
+}
+console.log(invest)
